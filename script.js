d6a6cb523851b5273f9d17789917c188612b16a9
shareemotion1112 <shareemotion1112@gmail.com>
2025-11-01 21:41:55 +0900

    feat: 애니메이션 감도 개선 및 프로젝트 그리드 레이아웃 수정
    - Intersection Observer의 threshold 값을 0.1에서 0.05로 낮추어 더 빠른 애니메이션 트리거 구현
    - rootMargin을 -50px에서 -10px로 조정하여 스크롤 반응성 향상
    - 프로젝트 섹션의 그리드 레이아웃을 동적 크기에서 3열 고정 레이아웃으로 변경
    - CSS 코드 포맷팅 개선 (멀티라인 속성값 한줄로 통일)
    - 불필요한 공백 및 따옴표 스타일 일

--- script.js
+++ script.js
@@ -62,8 +62,8 @@ window.addEventListener('scroll', () => {
 
 // Intersection Observer for fade-in animations
 const observerOptions = {
-    threshold: 0.1,
-    rootMargin: '0px 0px -50px 0px'
+    threshold: 0.05,
+    rootMargin: '0px 0px -10px 0px'
 };
 
 const observer = new IntersectionObserver((entries) => {
